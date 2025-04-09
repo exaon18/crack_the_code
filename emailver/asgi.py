874fe95c3518fabc64bafe8a48ce5a4176e09fd3
exaon18 <exaontech@gamil.com>
2025-04-09 05:15:48 -0700

    i hope its final

--- emailver/asgi.py
+++ emailver/asgi.py
@@ -1,13 +1,17 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
+import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from Games import routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+# Set the Django settings module FIRST
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emailver.settings')
+
+# Initialize Django application BEFORE importing consumers/routing
+django_asgi_app = get_asgi_application()
+
+# Now import WebSocket routes (which may reference models/consumers)
+from Games.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
+    "http": django_asgi_app,
+    "websocket": URLRouter(websocket_urlpatterns),
 })
